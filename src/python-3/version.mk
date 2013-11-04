b5c4ec2ce546ede5df7262d0c63870b21a0f3c50
Trevor Cooper <tcooper@sdsc.edu>
2013-11-04 14:24:56 -0800

    Added pull.mk to src/python-3 and modified Makefile/version.mk to use it

--- src/python-3/version.mk
+++ src/python-3/version.mk
@@ -4,3 +4,11 @@ ARCHIVENAME    	= Python
 VERSION 	= 3.2.1
 RELEASE 	= 2
 TARBALL_POSTFIX	= tgz
+
+SRC_SUBDIR	= python-3
+
+PYTHON3_NAME	= $(ARCHIVENAME)
+PYTHON3_VERSION	= $(VERSION)
+
+# List of *.tgz packages that are part of THIS package
+TGZ_PKGS = $(PYTHON3_NAME)-$(PYTHON3_VERSION).$(TARBALL_POSTFIX)
