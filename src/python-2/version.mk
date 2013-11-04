b51ef376cccfdb4c8f93a412f831e1faa19bb48f
Trevor Cooper <tcooper@sdsc.edu>
2013-11-04 14:26:14 -0800

    Added src/python-2/[pull.mk|binary_hashes] and modified src/python-2/[Makefile|version.mk] to use them

--- src/python-2/version.mk
+++ src/python-2/version.mk
@@ -4,3 +4,11 @@ ARCHIVENAME    	= Python
 VERSION 	= 2.7.3
 RELEASE 	= 2
 TARBALL_POSTFIX	= tgz
+
+SRC_SUBDIR	= python-2
+
+PYTHON2_NAME	= $(ARCHIVENAME)
+PYTHON2_VERSION	= $(VERSION)
+
+# List of *.tgz packages that are part of THIS package
+TGZ_PKGS = $(PYTHON2_NAME)-$(PYTHON2_VERSION).$(TARBALL_POSTFIX)
