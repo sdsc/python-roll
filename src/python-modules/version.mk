d61c78c1f2703c24628045296e1b94391c889ecb
Jim Hayes <jhayes@sdsc.edu>
2014-09-28 18:37:36 -0700

    Determine Python versions in modulefiles programmatically.

--- src/python-modules/version.mk
+++ src/python-modules/version.mk
@@ -1,6 +1,11 @@
-NAME       = opt-python-modules
-VERSION    = 1
-RELEASE    = 3
-PKGROOT    = /opt/modulefiles/compilers/python
+NAME         = opt-python-modules
+VERSION      = 1
+RELEASE      = 4
+PKGROOT      = /opt/modulefiles/compilers/python
 
-RPM.EXTRAS = AutoReq:No
+VERSION2_SRC = $(REDHAT.ROOT)/src/python-2/version.mk
+VERSION3_SRC = $(REDHAT.ROOT)/src/python-3/version.mk
+VERSION_INC  = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS   = AutoReq:No
