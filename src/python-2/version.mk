d8817cf257af857baef72e31db768decd3227e51
Jerry Greenberg <jpg@sdsc.edu>
2015-10-07 17:45:06 -0700

    updated all packages where updates were available

--- src/python-2/version.mk
+++ src/python-2/version.mk
@@ -4,8 +4,8 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME               = sdsc-python-27
-VERSION            = 2.7.9
-RELEASE            = 2
+VERSION            = 2.7.10
+RELEASE            = 0
 PKGROOT            = /opt/python
 
 SRC_SUBDIR         = python-2
@@ -18,25 +18,25 @@ SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 DATEUTILS_NAME     = python-dateutil
 DATEUTILS_SUFFIX   = tar.gz
-DATEUTILS_VERSION  = 2.4.0
+DATEUTILS_VERSION  = 2.4.2
 DATEUTILS_PKG      = $(DATEUTILS_NAME)-$(DATEUTILS_VERSION).$(DATEUTILS_SUFFIX)
 DATEUTILS_DIR      = $(DATEUTILS_PKG:%.$(DATEUTILS_SUFFIX)=%)
 
 NOSE_NAME          = nose
 NOSE_SUFFIX        = tar.gz
-NOSE_VERSION       = 1.3.4
+NOSE_VERSION       = 1.3.7
 NOSE_PKG           = $(NOSE_NAME)-$(NOSE_VERSION).$(NOSE_SUFFIX)
 NOSE_DIR           = $(NOSE_PKG:%.$(NOSE_SUFFIX)=%)
 
 NUMPY_NAME         = numpy
 NUMPY_SUFFIX       = tar.gz
-NUMPY_VERSION      = 1.9.1
+NUMPY_VERSION      = 1.10.0
 NUMPY_PKG          = $(NUMPY_NAME)-$(NUMPY_VERSION).$(NUMPY_SUFFIX)
 NUMPY_DIR          = $(NUMPY_PKG:%.$(NUMPY_SUFFIX)=%)
 
 PIP_NAME           = pip
 PIP_SUFFIX         = tar.gz
-PIP_VERSION        = 1.5.6
+PIP_VERSION        = 7.1.2
 PIP_PKG            = $(PIP_NAME)-$(PIP_VERSION).$(PIP_SUFFIX)
 PIP_DIR            = $(PIP_PKG:%.$(PIP_SUFFIX)=%)
 
@@ -48,25 +48,25 @@ PYPARSING_DIR      = $(PYPARSING_PKG:%.$(PYPARSING_SUFFIX)=%)
 
 PYTZ_NAME          = pytz
 PYTZ_SUFFIX        = tar.gz
-PYTZ_VERSION       = 2014.10
+PYTZ_VERSION       = 2015.6
 PYTZ_PKG           = $(PYTZ_NAME)-$(PYTZ_VERSION).$(PYTZ_SUFFIX)
 PYTZ_DIR           = $(PYTZ_PKG:%.$(PYTZ_SUFFIX)=%)
 
 SETUPTOOLS_NAME    = setuptools
 SETUPTOOLS_SUFFIX  = tar.gz
-SETUPTOOLS_VERSION = 7.0
+SETUPTOOLS_VERSION = 18.3.2
 SETUPTOOLS_PKG     = $(SETUPTOOLS_NAME)-$(SETUPTOOLS_VERSION).$(SETUPTOOLS_SUFFIX)
 SETUPTOOLS_DIR     = $(SETUPTOOLS_PKG:%.$(SETUPTOOLS_SUFFIX)=%)
 
 SIX_NAME           = six
 SIX_SUFFIX         = tar.gz
-SIX_VERSION        = 1.9.0
+SIX_VERSION        = 1.10.0
 SIX_PKG            = $(SIX_NAME)-$(SIX_VERSION).$(SIX_SUFFIX)
 SIX_DIR            = $(SIX_PKG:%.$(SIX_SUFFIX)=%)
 
 CYTHON_NAME        = Cython
 CYTHON_SUFFIX      = tar.gz
-CYTHON_VERSION     = 0.21.2
+CYTHON_VERSION     = 0.23.3
 CYTHON_PKG         = $(CYTHON_NAME)-$(CYTHON_VERSION).$(CYTHON_SUFFIX)
 CYTHON_DIR         = $(CYTHON_PKG:%.$(CYTHON_SUFFIX)=%)
 
