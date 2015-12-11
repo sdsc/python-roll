5697d1f3bd1954d17a896c1ed9b1899cb3a57f40
Jim Hayes <jhayes@sdsc.edu>
2015-12-11 08:30:23 -0800

    Bug fix: version substition in modulefile was giving wrong result. Resolves APS-222.

--- src/python-modules/version.mk
+++ src/python-modules/version.mk
@@ -2,10 +2,10 @@ PACKAGE      = python
 CATEGORY     = compilers
 
 NAME         = sdsc-$(PACKAGE)-modules
-RELEASE      = 9
+RELEASE      = 10
 PKGROOT      = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
-VERSION2_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-2/version.mk
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-2/version.mk
 VERSION3_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-3/version.mk
 VERSION_INC  = version.inc
 include $(VERSION_INC)
